c8f60a0c14a6b709873bfb362502ab3d189793fe
wil-bert <ianwilbuts@gmail.com>
2020-07-20 00:38:40 +0300

    Change background color

--- js/script.js
+++ js/script.js
@@ -32,8 +32,6 @@ function validate() {
     DD = parseInt(date.substring(0,2));
     d = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
     console.log(d);
-    d = calculateDayValue();
-
 
   var gender = document.querySelector('input[name="gender"]:checked').value;
   switch (gender){
